5f30065130c211aa888e6462a7b9db7b280d3c87
azgmohammadd <azgmohammadd@gmail.com>
2022-06-26 02:26:59 +0430

    add images to views

--- Project/Doc/images/Views/AUT_SUM.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW AUT_SUM AS (
-	SELECT aFname AS  'نام مولف', aLname AS 'تام خانوادگی مولف', SumPayment as 'حقوق مولف'
-	FROM Authors
-);
-
-SELECT * FROM AUT_SUM;
--- Project/Doc/images/Views/BOOK_PUB.sql
+++
@@ -1,9 +0,0 @@
-/* کتاب و ناشر*/
-CREATE VIEW BOOK_PUB AS (
-	SELECT Title AS 'عنوان کتاب', pName 'نام انتشارات'
-	FROM	(Publishers NATURAL JOIN Pubbook)
-	JOIN books USING (ISBN)
-	ORDER BY pName
-);
-
-SELECT * FROM BOOK_PUB;
