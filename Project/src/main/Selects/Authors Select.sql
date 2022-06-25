b54918f26f8dac4c9ba727bd6098e76d72d7a45e
azgmohammadd <azgmohammadd@gmail.com>
2022-06-25 15:58:48 +0430

    add images folder to selects

--- Project/src/main/Selects/Authors Select.sql
+++ Project/src/main/Selects/Authors Select.sql
@@ -7,11 +7,17 @@ FROM	Authors;
 
 /* پیر ترین نویسنده */
 SELECT	atID as 'کد مولف', aFname as 'نام مولف' , aLname as 'نام خانوادگی مولف' , MAX(Age) as 'بیشترین سن'
-FROM	Authors;
+FROM	Authors
+WHERE 	Age = (
+	SELECT	MAX(Age)
+	FROM	Authors);
 
  /* جوان ترین نویسنده */
 SELECT	atID as 'کد مولف', aFname as 'نام مولف' , aLname as 'نام خانوادگی مولف' , MIN(Age) as 'کمترین سن'
-FROM	Authors;
+FROM	Authors
+WHERE	Age = (
+	SELECT	MIN(Age)
+    FROM	Authors);
 
 /* میانگین سن نویسنده ها */
 SELECT	AVG(Age) as 'میانگین سن'
@@ -19,16 +25,68 @@ FROM	Authors;
 
 /* بیشترین حق تالیف*/
 SELECT atID as 'کد مولف', aFname as 'نام مولف', aLname as 'نام خانوادگی مولف', MAX(SumPayment) as 'بیشترین حق تالیف'
-FROM Authors;
+FROM Authors
+WHERE SumPayment = (
+	SELECT	MAX(SumPayment)
+	FROM	Authors);
 
 /*کمترین حق تالیف */
 SELECT atID as 'کد مولف', aFname as 'نام مولف', aLname as 'نام خانوادگی مولف', MIN(SumPayment) as 'کمترین حق تالیف'
-FROM Authors;
+FROM Authors
+WHERE SumPayment = (
+	SELECT	MIN(SumPayment)
+	FROM	Authors);
 
 /* میانگین  درآمد  نویسنده ها */
-SELECT	AVG(SumPaymnet) as 'میانگین درآمد نویسنده ها'
+SELECT	AVG(SumPayment) as 'میانگین درآمد نویسنده ها'
 FROM	Authors;
 
 /*  مجموع درآمد نویسنده ها*/
-SELECT	SUM(SumPaymnet) as 'مجموع درآمد نویسنده ها'
+SELECT	SUM(SumPayment) as 'مجموع درآمد نویسنده ها'
 FROM	Authors;
+
+/*مجموع حق نشر هر ناشر*/
+SELECT SUM(pubbook.payment) AS 'مجموع حق نشر هر ناشر', pName AS 'نام ناشر'
+FROM ((( publishers NATURAL JOIN pubbook) JOIN books USING (isbn))
+JOIN authbook USING (isbn))
+JOIN authors USING (atID)
+GROUP BY pName;
+
+/* کتاب و ناشر*/
+SELECT Title, pName
+FROM	(Publishers NATURAL JOIN Pubbook)
+JOIN books USING (ISBN)
+ORDER BY pName;
+
+/*هر نویسنده با چه ناشر هایی همکاری کرده است.*/
+SELECT DISTINCT pName AS 'نام ناشر', aLname AS 'نام نویسنده'
+FROM	(((publishers NATURAL JOIN Pubbook)
+JOIN Books USING (ISBN))
+JOIN Authbook USING (ISBN))
+JOIN Authors USING(atID)
+WHERE aLname = 'معروفی';
+
+/*هر ناشر چه گروهی از کتاب ها را منتشر می کند*/
+SELECT DISTINCT groupName AS ' نام گروه هایی که نشر نیلوفر چاپ می کند'
+FROM ((Publishers NATURAL JOIN Pubbook)
+JOIN books USING (ISBN))
+JOIN groupbook USING (GroupID)
+WHERE pname = 'نیلوفر';
+
+/*هر نویسنده چه گروهی از کتاب ها را نوشته است*/
+SELECT DISTINCT groupName AS ' نام گروه هایی که بولگاف نوشته است'
+FROM ((Authors JOIN Authbook USING (atID))
+JOIN books USING (ISBN))
+JOIN groupbook USING (GroupID)
+WHERE atID = 490194;
+
+/*نام ناشر هایی که کتاب با ژانر ایرانی یا روسی چاپ کردند*/
+(SELECT pName AS ' نام ناشر هایی که کتاب با ژانر ایرانی یا روسی چاپ کردند'
+FROM ((Publishers NATURAL JOIN pubbook) JOIN books USING (ISBN))
+JOIN groupbook USING (groupID)
+WHERE groupname = 'روسی')
+UNION
+(SELECT pName
+FROM ((Publishers NATURAL JOIN pubbook JOIN books USING (ISBN))
+JOIN groupBook USING (groupID))
+WHERE groupName = 'ایرانی'); 
