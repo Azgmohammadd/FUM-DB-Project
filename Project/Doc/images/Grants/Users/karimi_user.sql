1882732e920c49fa85be654e587a90f869fc020b
azgmohammadd <azgmohammadd@gmail.com>
2022-06-25 21:09:00 +0430

    add users and roles queries images

--- Project/Doc/images/Grants/Users/karimi_user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'karimi'@'localhost' IDENTIFIED BY 'password';
-
-GRANT Publisher_role TO 'karimi'@'localhost';
--- Project/Doc/images/Grants/Users/maroofi_user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'maroofi'@'localhost' IDENTIFIED BY 'password';
-
-GRANT Authors_role TO 'maroofi'@'localhost';
