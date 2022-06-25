30632fcab5de16eb2ebfc73b380c7870db1bdc9e
azgmohammadd <azgmohammadd@gmail.com>
2022-06-25 21:07:02 +0430

    add users queries images

--- Project/Doc/images/Grants/Roles/Authors_role.sql
+++
@@ -1,5 +0,0 @@
-CREATE ROLE Authors_role;
-
-GRANT SELECT, CREATE, UPDATE, DELETE ON fum_database.PUB_AUT TO Authors_role;
-
-GRANT SELECT, CREATE, UPDATE, DELETE ON fum_database.AUT_SUM TO Authors_role;
--- Project/Doc/images/Grants/Roles/Publisher_role.sql
+++
@@ -1,7 +0,0 @@
-CREATE ROLE Publishers_role;
-
-GRANT SELECT, CREATE, UPDATE, DELETE ON fum_database.PUB_INFO TO Publishers_role;
-
-GRANT SELECT, CREATE, UPDATE, DELETE ON fum_database.PUB_SUM TO Publishers_role;
-
-GRANT SELECT, CREATE, UPDATE, DELETE ON fum_database.BOOK_PUB TO Publishers_role;
