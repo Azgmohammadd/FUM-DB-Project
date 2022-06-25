88d6e01e08597cdf7052ee5bda9131baf06b7c57
azgmohammadd <azgmohammadd@gmail.com>
2022-06-25 20:20:41 +0430

    add users

---
+++ Project/src/main/Grants/Users/karimi_user.sql
@@ -0,0 +1,3 @@
+CREATE USER 'karimi'@'localhost' IDENTIFIED BY 'password';
+
+GRANT Publisher_role TO 'karimi'@'localhost';
---
+++ Project/src/main/Grants/Users/maroofi_user.sql
@@ -0,0 +1,3 @@
+CREATE USER 'maroofi'@'localhost' IDENTIFIED BY 'password';
+
+GRANT Authors_role TO 'maroofi'@'localhost';
